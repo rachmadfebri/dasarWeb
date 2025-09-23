89fd6572ec9e9a6a2731a8f68efb493fa493e3c1
Rachmad Febriananda <rachmad17845@gmail.com>
2025-09-23 20:58:31 +0700

    add operator penugasan

--- operator.php
+++ operator.php
@@ -21,6 +21,20 @@ $hasilOr = $a || $b;
 $hasilNotA = !$a;
 $hasilNotB = !$b;
 
+// Operator Penugasan
+$x = 10;
+$y = 5;
+
+$hasilTambahTugas = ($x += $y);
+$x = 10; // reset
+$hasilKurangTugas = ($x -= $y);
+$x = 10;
+$hasilKaliTugas = ($x *= $y);
+$x = 10;
+$hasilBagiTugas = ($x /= $y);
+$x = 10;
+$hasilSisaTugas = ($x %= $y);
+
 echo "a = 10, b = 5 <br>";
 echo "Hasil Tambah = {$hasilTambah} <br>";
 echo "Hasil Kurang = {$hasilKurang} <br>";
@@ -31,7 +45,7 @@ echo "Sisa Bagi = {$sisaBagi} <br>";
 echo "Hasil Sama = {$hasilSama} <br>";
 echo "Hasil Tidak Sama = {$hasilTidakSama} <br>";
 echo "Hasil Lebih Kecil = {$hasilLebihKecil} <br>";
-echo "HasilLebihBesar = {$hasilLebihBesar} <br>";
+echo "Hasil Lebih Besar = {$hasilLebihBesar} <br>";
 echo "Hasil Lebih Kecil Sama = {$hasilLebihKecilSama} <br>";
 echo "Hasil Lebih Besar Sama = {$hasilLebihBesarSama} <br>";
 
@@ -39,4 +53,10 @@ echo "Hasil And = {$hasilAnd} <br>";
 echo "Hasil Or = {$hasilOr} <br>";
 echo "Hasil Not A = {$hasilNotA} <br>";
 echo "Hasil Not B = {$hasilNotB} <br>";
+
+echo "Penugasan += : {$hasilTambahTugas}<br>";
+echo "Penugasan -= : {$hasilKurangTugas}<br>";
+echo "Penugasan *= : {$hasilKaliTugas}<br>";
+echo "Penugasan /= : {$hasilBagiTugas}<br>";
+echo "Penugasan %= : {$hasilSisaTugas}<br>";
 ?>
