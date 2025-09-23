e3618d29031ba095ba569fa6b6b4bbdbf8808157
Rachmad Febriananda <rachmad17845@gmail.com>
2025-09-23 23:34:35 +0700

    add foreach + if

--- struktur_kontrol.php
+++ struktur_kontrol.php
@@ -48,4 +48,17 @@ foreach ($skorUjian as $skor) {
 }
 
 echo "Total skor ujian adalah: $totalSkor";
+
+echo "<br>";
+echo "<br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+  if ($nilai < 60) {
+    echo "Nilai: $nilai (Tidak lulus) <br>";
+    continue;
+  }
+  echo "Nilai: $nilai (Lulus) <br>";
+}
 ?>
