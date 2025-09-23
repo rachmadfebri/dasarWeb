642bf8a794828ad61fc5bf58924b16f63103dd96
Rachmad Febriananda <rachmad17845@gmail.com>
2025-09-23 19:34:23 +0700

    add operator pembanding

--- operator.php
+++ operator.php
@@ -9,11 +9,24 @@ $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
 
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
 echo "a = 10, b = 5 <br>";
 echo "Hasil Tambah = {$hasilTambah} <br>";
 echo "Hasil Kurang = {$hasilKurang} <br>";
 echo "Hasil Kali = {$hasilKali} <br>";
 echo "Hasil Bagi = {$hasilBagi} <br>";
 echo "Sisa Bagi = {$sisaBagi} <br>";
-echo "Pangkat = {$pangkat} <br>";
+
+echo "Hasil Sama = {$hasilSama} <br>";
+echo "Hasil Tidak Sama = {$hasilTidakSama} <br>";
+echo "Hasil Lebih Kecil = {$hasilLebihKecil} <br>";
+echo "HasilLebihBesar = {$hasilLebihBesar} <br>";
+echo "Hasil Lebih Kecil Sama = {$hasilLebihKecilSama} <br>";
+echo "Hasil Lebih Besar Sama = {$hasilLebihBesarSama} <br>";
 ?>
